05d89502f8845caa8890a5b756b80c607bf00bb8
PixelwarStudio <janko.matthes@gmx.net>
2019-03-22 00:01:53 +0100

    adjust/add values

--- game_conf.lua
+++ game_conf.lua
@@ -1,8 +1,12 @@
 local game_conf = {}
 
 game_conf.field = {}
-game_conf.field.width = 23
-game_conf.field.height = 15
-game_conf.field.cell_size = 40
+game_conf.field.width = 15
+game_conf.field.height = 10
+game_conf.field.cell_size = 60
+
+game_conf.font = {}
+game_conf.font.title = 100
+game_conf.font.entry = 80
 
 return game_conf
